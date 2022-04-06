7fddc0d390391cf4e08f90183bad1e457c22365e
opajkovic <oljaolja170@gmail.com>
2022-04-06 19:57:37 +0200

    added ex10

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -1,3 +1,29 @@
 function myFunction() {
-    
+    var myMusic = {
+        001 : {
+            artist : "Billy Joel",
+            title: "Piano Man",
+            release_year: "1973",
+            formats: {
+                1: "CD",
+                2: "8T",
+                3: "LP"
+            },
+            gold: true
+        },
+        002 : {
+            artist : "Joe Cocker",
+            title: "Unchain My Heart",
+            release_year: "1987",
+            formats: {
+                1: "CD",
+                2: "8T",
+                3: "LP"
+            },
+            gold: true
+        }
+    };
+    return myMusic;
 }
+console.log(myFunction()[2]);
+module.exports = myFunction;
