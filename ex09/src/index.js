3d5f5be3f2b171560a82e5bd9e3facf904f55036
opajkovic <oljaolja170@gmail.com>
2022-04-07 07:26:38 +0200

    added

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -4,4 +4,8 @@ function myFunction(myObj, checkProp) {
     }
     return "Not found";
 }
+console.log(myFunction({title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "title"));
+console.log(myFunction({title: "Titanic", song: 'My Heart Will Go On', genre: "drama"}, "song"));
+console.log(myFunction({title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "genre"));
+console.log(myFunction({title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "actor"));
 module.exports= myFunction;
